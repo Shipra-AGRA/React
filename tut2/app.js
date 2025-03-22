3299e5b9e7094d6c2d592a35bca592a164d5a6c8
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-03-23 01:43:18 +0530

    learn about parcel and folder structure

---
+++ tut2/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const main = React.createElement("h3",{},"Hello Using React")
+root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(main)
